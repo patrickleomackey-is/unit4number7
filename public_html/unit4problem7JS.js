7b5126239e347b26fd8d2dbd31734d182d30bddc
patrickleomackey-is <plmackey@my.niacc.edu>
2020-10-19 10:22:19 -0500

    This is the final product of LoopsLoops assignment 10/15/2020

---
+++ public_html/unit4problem7JS.js
@@ -0,0 +1,8 @@
+/* Patrick Mackey CIS 119 Prof. K. Miller unit4problem7*/
+let dailySales = [];
+let salesTotal = 0;
+for (let i = 0; i <= 6;i++){
+ dailySales.push(Number(prompt("Enter the daily sales total:")));   
+salesTotal += dailySales[i];
+}
+document.write("$"+  salesTotal.toFixed(2) + " is this weeks total sales");
